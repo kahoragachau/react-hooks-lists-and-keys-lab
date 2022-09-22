ee0ab49b8291f57f2befd9d9c441d2bb9d4ef327
kahoragachau <joelkahoragachau6@gmail.com>
2022-09-22 16:25:55 +0300

    - Pass ProjectList test

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,16 +1,21 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  // const technologiesArray = technologies.map((technology) => {
-  //   return <span>{technology}</span>
-  // })
+  const technologiesArray = technologies.map((technology) => {
+    console.log(technology)
+    return(
+      <div>
+        <span key={technology}>{technology}</span>
+       </div>
+       )
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
-        {/* {technologiesArray} */}
+        {technologiesArray}
       </div>
     </div>
   );
