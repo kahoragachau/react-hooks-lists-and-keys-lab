d6b4b3d194cd2209eb6750331907e5c4ceb6090a
kahoragachau <joelkahoragachau6@gmail.com>
2022-09-22 15:09:23 +0300

    - Pass Navbar test

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,13 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const projectsArray = projects.map((project) => {
+    <ProjectItem key={project.id}/>
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */}{projectsArray}</div>
     </div>
   );
 }
